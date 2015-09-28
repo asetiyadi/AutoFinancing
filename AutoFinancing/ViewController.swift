678941d21b81e72e80ee993a261df4893645ab6c
Andi Setiyadi <setiyadi@us.ibm.com>
2015-09-28 16:05:01 -0600

    Initial Commit

---
+++ AutoFinancing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoFinancing
+//
+//  Created by Andi Setiyadi on 9/28/15.
+//  Copyright © 2015 Andi Setiyadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
